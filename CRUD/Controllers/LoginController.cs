12c1360ac6b2f656471274fba59a4e9a8f7475c1
Nikhil Pawar <nikhilapawar27@gmail.com>
2023-09-02 11:49:57 +0530

    Initial commit

---
+++ CRUD/Controllers/LoginController.cs
@@ -0,0 +1,75 @@
+﻿using CRUD.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class LoginController : Controller
+    {
+        Db_TestEntities1 dbobj = new Db_TestEntities1();
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+        public ActionResult checkLogin(tbl_login model)
+        {
+            using (var context = new Db_TestEntities1())
+            {
+                bool isValid = context.tbl_login.Any(x=>x.Email == model.Email && x.Password==model.Password);
+                if(isValid)
+                {
+                    TempData["AlertMessage"] = "Login successfully";
+                    return RedirectToAction("Student","Student");
+                }
+                ModelState.AddModelError("","Invalid Email and Password");
+                return View("Login");
+            }
+               
+        }
+
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddUser(tbl_login model)
+        {
+            tbl_login obj = new tbl_login();
+
+            using (var con = new Db_TestEntities1())
+            {
+                bool isValid = con.tbl_login.Any(x => x.Email == model.Email);
+                if (isValid)
+                {
+                    TempData["AlertMessage"] = "User Already created";
+                    return View("Register");
+                }
+                else
+                {
+
+                    if (ModelState.IsValid)
+                    {
+                        //obj.Id = model.Id;
+                        obj.Email = model.Email;
+                        obj.Password = model.Password;
+                        dbobj.tbl_login.Add(obj);
+                        dbobj.SaveChanges();
+                        TempData["AlertMessage"] = "User Created successfully";
+                    }
+                    ModelState.Clear();
+                    return View("Login");
+                }
+            }
+
+        } 
+        
+    }
+}
---
+++ CRUD/Controllers/StudentController.cs
@@ -0,0 +1,73 @@
+﻿using CRUD.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class StudentController : Controller
+    {
+        Db_TestEntities dbobj = new Db_TestEntities();
+        public ActionResult Student(tbl_student obj)
+        {
+            
+                return View(obj);
+        }
+        [HttpPost]
+        public ActionResult AddStudent(tbl_student model)
+        {
+            tbl_student obj = new tbl_student();
+            if (ModelState.IsValid)
+            {
+                
+                obj.Id = model.Id;
+                obj.Name = model.Name;
+                obj.Fname = model.Fname;
+                obj.Email = model.Email;
+                obj.Mobile = model.Mobile;
+                obj.Description = model.Description;
+                if (model.Id == 0)
+                {
+                    dbobj.tbl_student.Add(obj);
+                    dbobj.SaveChanges();
+                    TempData["AlertMessage"] = "Student Added successfully";
+                }
+                else
+                {
+                    dbobj.Entry(obj).State = EntityState.Modified;
+                    dbobj.SaveChanges();
+                    TempData["AlertMessage"] = "Student Modified successfully";
+                }           
+          }
+            ModelState.Clear();
+            return View("Student");
+        }
+
+        public ActionResult StudentList()
+        {
+            var res = dbobj.tbl_student.ToList();
+            return View(res);
+        }
+        public ActionResult Delete(int id)
+        {
+            var res = dbobj.tbl_student.Where(x => x.Id == id).First();
+            dbobj.tbl_student.Remove(res);
+            dbobj.SaveChanges();
+            TempData["AlertMessage"] = "Student Delete successfully";
+            var list = dbobj.tbl_student.ToList();       
+            return View("StudentList",list);
+           
+        }
+
+
+
+
+
+
+
+
+    }
+}
